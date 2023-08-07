a82a63f2d8cd45a6e15932f6644935dc95e7877b
Luke Bakken <luke@bakken.io>
2023-08-07 13:35:20 -0700

    Getting setup

--- run-rabbitmq.ps1
+++ run-rabbitmq.ps1
@@ -1,68 +1,46 @@
-$DebugPreference = "Continue"
-$ErrorActionPreference = 'Stop'
 $ProgressPreference = 'Continue'
-# Set-PSDebug -Strict -Trace 1
-Set-PSDebug -Off
-Set-StrictMode -Version 'Latest' -ErrorAction 'Stop' -Verbose
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 2.0
 
-New-Variable -Name curdir  -Option Constant -Value $PSScriptRoot
-Write-Host "[INFO] curdir: $curdir"
+New-Variable -Name curdir -Option Constant `
+  -Value (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+
+Write-Host "[INFO] script directory: $curdir"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor 'Tls12'
 
-New-Variable -Name rmq_version -Option Constant -Value '3.11.10'
+New-Variable -Name rabbitmq_version -Option Constant -Value '3.12.2'
 
-# https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.11.10/rabbitmq-server-windows-3.11.10.zip
-New-Variable -Name rmq_zip_download_url -Option Constant -Value "https://github.com/rabbitmq/rabbitmq-server/releases/download/v$rmq_version/rabbitmq-server-windows-$rmq_version.zip"
-New-Variable -Name rmq_zip_file -Option Constant -Value (Join-Path -Path $curdir -ChildPath "rabbitmq-server-windows-$rmq_version.zip")
+$rabbitmq_dir = Join-Path -Path $curdir -ChildPath "rabbitmq_server-$rabbitmq_version"
+$rabbitmq_sbin = Join-Path -Path $rabbitmq_dir -ChildPath 'sbin'
+$rabbitmq_download_url = "https://github.com/rabbitmq/rabbitmq-server/releases/download/v$rabbitmq_version/rabbitmq-server-windows-$rabbitmq_version.zip"
+$rabbitmq_zip_file = Join-Path -Path $curdir -ChildPath "rabbitmq-server-windows-$rabbitmq_version.zip"
+$rabbitmq_plugins_cmd = Join-Path -Path $rabbitmq_sbin -ChildPath 'rabbitmq-plugins.bat'
+$rabbitmq_server_cmd = Join-Path -Path $rabbitmq_sbin -ChildPath 'rabbitmq-server.bat'
 
-if (-Not (Test-Path -Path $rmq_zip_file))
-{
-    Invoke-WebRequest -UseBasicParsing -Uri $rmq_zip_download_url -OutFile $rmq_zip_file
-}
-else
-{
-    Write-Host "[INFO] found '$rmq_zip_file'"
-}
+$rabbitmq_base = Join-Path -Path $curdir -ChildPath 'rmq'
+$rabbitmq_conf_in = Join-Path -Path $curdir -ChildPath 'rabbitmq.conf.in'
+$rabbitmq_conf_out = Join-Path -Path $rabbitmq_base -ChildPath 'rabbitmq.conf'
 
-New-Variable -Name rmq_dir -Option Constant -Value (Join-Path -Path $curdir -ChildPath "rabbitmq_server-$rmq_version")
-
-if (-Not (Test-Path -Path $rmq_dir -Type Container))
+if (!(Test-Path -Path $rabbitmq_dir))
 {
-    Expand-Archive -Path $rmq_zip
-}
-else
-{
-    Write-Host "[INFO] found '$rmq_dir'"
+    New-Item -Path $rabbitmq_base -ItemType Directory
+    Invoke-WebRequest -Verbose -UseBasicParsing -Uri $rabbitmq_download_url -OutFile $rabbitmq_zip_file
+    Expand-Archive -Path $rabbitmq_zip_file -DestinationPath $curdir
+    & $rabbitmq_plugins_cmd enable rabbitmq_management
 }
 
-New-Variable -Name rmq_base_dir -Option Constant -Value (Join-Path -Path $curdir -ChildPath 'rabbitmq')
-New-Variable -Name rmq_log_dir -Option Constant -Value (Join-Path -Path $rmq_base_dir -ChildPath 'log')
-New-Variable -Name rmq_config_file -Option Constant -Value (Join-Path -Path $curdir -ChildPath 'rabbitmq.conf')
-New-Variable -Name rmq_enabled_plugins_file -Option Constant -Value (Join-Path -Path $rmq_base_dir -ChildPath 'enabled_plugins')
+$pwd_slashes = $curdir -Replace '\\','/'
+(Get-Content -Raw -Path $rabbitmq_conf_in) -Replace '@@PWD@@', $pwd_slashes | Set-Content -Path $rabbitmq_conf_out
 
 try
 {
     $env:RABBITMQ_ALLOW_INPUT = 'true'
-    $env:RABBITMQ_BASE = $rmq_base_dir
-    $env:RABBITMQ_CONFIG_FILE = $rmq_config_file
-
-    Remove-Item -Force $rmq_log_dir/*
-
-    if (-Not (Test-Path -Path $rmq_enabled_plugins_file))
-    {
-        & "$rmq_dir/sbin/rabbitmq-plugins.bat" enable rabbitmq_management rabbitmq_top
-    }
-    else
-    {
-        Write-Host "[INFO] found '$rmq_enabled_plugins_file'"
-    }
-
-    & "$rmq_dir/sbin/rabbitmq-server.bat"
+    $env:RABBITMQ_BASE = $rabbitmq_base
+    & $rabbitmq_server_cmd
 }
 finally
 {
     Remove-Item -Path env:\RABBITMQ_ALLOW_INPUT
     Remove-Item -Path env:\RABBITMQ_BASE
-    Remove-Item -Path env:\RABBITMQ_CONFIG_FILE
 }
--- setup.ps1
+++ setup.ps1
@@ -5,17 +5,11 @@ Set-StrictMode -Version 2.0
 New-Variable -Name curdir -Option Constant `
   -Value (Split-Path -Parent $MyInvocation.MyCommand.Definition)
 
+& openssl x509 -in ./certs/ca_certificate.pem -out ./certs/ca_certificate.crt
+
+& openssl pkcs12 -inkey ./certs/client_localhost_key.pem -in ./certs/client_localhost_certificate.pem -certfile ./certs/ca_certificate.pem -export -out ./certs/client_localhost_certificate.pfx
+
 New-Variable -Name ca_cert_file -Option Constant `
   -Value (Join-Path -Path $curdir -ChildPath 'certs' | Join-Path -ChildPath 'ca_certificate.crt')
 
 Import-Certificate -Verbose -FilePath $ca_cert_file -CertStoreLocation cert:\CurrentUser\Root
-
-New-Variable -Name projdir -Option Constant `
-  -Value (Join-Path -Path $curdir -ChildPath 'RabbitMQ.Explore')
-
-New-Variable -Name appsettings_json -Option Constant `
-  -Value (Join-Path -Path $projdir -ChildPath 'appsettings.json')
-
-New-Variable -Name curdir2 -Option Constant `
-  -Value ($curdir -Replace '\\','\\')
-(Get-Content -Raw -Path $appsettings_json) -Replace '..\\\\..\\\\..\\\\..', $curdir2 | Set-Content -Path $appsettings_json
